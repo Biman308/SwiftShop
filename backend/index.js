db79cfa682874e905cfc9c5f8cdeacc1dc351b21
Biman Patra <119288685+Biman308@users.noreply.github.com>
2025-02-17 11:51:04 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -6,17 +6,25 @@ const connectDB = require("./config/db");
 const router = require("./routes");
 
 const app = express();
-
 app.use(
   cors({
     origin: process.env.FRONTEND_URL,
+    // origin: '',
     credentials: true,
   })
 );
 app.use(express.json());
 app.use(cookieParser());
+
 app.use("/api", router);
 
-connectDB();
+const PORT = 8080 || process.env.PORT;
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("connnect to DB");
+    console.log("Server is running " + PORT);
+  });
+});
 
 module.exports = app;
